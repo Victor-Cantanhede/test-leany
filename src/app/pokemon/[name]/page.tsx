e60152ab998e3f37e797d12103441c1a56763ffc
Victor Mateus Mesquita Cantanhede <victorcantanhedblk@gmail.com>
2025-06-05 10:33:29 -0300

    fix

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,16 +1,17 @@
-import axios from 'axios';
 import { notFound } from 'next/navigation';
+import axios from 'axios';
 import PokemonDetailClient from './PokemonDetailClient';
 
-type PageProps = {
-    params: {
-        name: string;
-    };
+
+interface PokemonDetailPageProps {
+  params: Promise<{ name: string }>;
 }
 
-export default async function PokemonDetailPage({ params }: PageProps) {
+export default async function PokemonDetailPage({ params }: PokemonDetailPageProps) {
+    const { name } = await params;
+
     try {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
         const p = res.data;
     
         return (
